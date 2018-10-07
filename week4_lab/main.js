9d43c737e2d05318ee51eb650676b3a72cfaba98
AmandaKennedy <43348070+AmandaKennedy@users.noreply.github.com>
2018-10-07 18:37:35 -0400

    Week 5 Homework

---
+++ week4_lab/main.js
@@ -0,0 +1,11 @@
+<html>
+    <head>
+    <!-- javascript -->
+<script src="main.js" type="text/javascript"></script>
+</head>
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+
+</html>
