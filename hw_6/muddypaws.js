3e8044a5c3ae041ee8fcc8e93a9ddb8431203708
AmandaKennedy <43348070+AmandaKennedy@users.noreply.github.com>
2018-10-19 14:41:34 -0400

    adding hw6

---
+++ hw_6/muddypaws.js
@@ -0,0 +1,6 @@
+<!-- javascript --><script src="muddypaws.js" type="text/javascript"></script>
+
+function addToCart() {
+    alert('Your item has been added to your cart!');    
+    console.log('hello world console');
+}
